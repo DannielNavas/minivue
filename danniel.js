8c07847f5ce5a8de573c9d915e0e0e9c8e0d9d3d
DannielNavas <dannielnavas@gmail.com>
2023-07-06 07:35:00 -0500

    ✨ proxy

--- danniel.js
+++ danniel.js
@@ -1,11 +1,25 @@
 class DannielReactive {
   constructor(options) {
     this.origin = options.data();
+
+    // destination
+
+    this.$data = new Proxy(this.origin, {
+      // TODO: modifica los datos dentro de js
+      get(target, name) {
+        console.log("get", target, name);
+        if (name in target) {
+          return target[name];
+        }
+        console.warn(`Propiedad ${name} no existe`);
+        return "";
+      },
+    });
   }
 
   mount() {
     document.querySelectorAll("*[dn-text]").forEach((element) => {
-      this.dnText(element, this.origin, element.getAttribute("dn-text"));
+      this.dnText(element, this.$data, element.getAttribute("dn-text"));
     });
   }
 
